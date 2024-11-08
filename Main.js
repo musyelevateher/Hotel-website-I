1c49902a66884ac94c9e8dc4daffa4993aa126d1
musyelevateher <musayyiba@gmail.com>
2024-11-08 11:27:12 +0100

    Added javascript to section

--- Main.js
+++ Main.js
@@ -6,3 +6,47 @@ const navMenu = document.getElementById("navMenu");
 hamburger.addEventListener("click", () => {
     navMenu.classList.toggle("active");
 });
+const chooseusContainer = [
+{  
+    Image: "pictures/payment.svg",
+    title: "Payment methods",
+    description: "We have a lot of them, from cryptocurrencies to barter for potatoes",
+ }
+ {
+    Image: "pictures/search.svg" ,
+    title: "Simple search process" ,
+    description: "We checked it out, even the kid did it, but it was my mom's friend's son" ,
+}
+{
+    Image: "pictures/support.svg" ,
+    title: "24/7 Support" ,
+    description: "Is there something you don't understand? Feel free to call us. Phone number in the footer" ,
+}
+{
+    Image: "pictures/mice.svg" ,
+    title: "We are nice" ,
+    description: "Fantasy is over, there will be something really convincing here" ,
+}
+]
+
+const chooseusContainer = [
+        {  Image:"pictures/payment.svg", title: "Payment methods" , description: "We have a lot of them, from cryptocurrencies to barter for potatoes" },
+        {  Image: "pictures/search.svg", title: "Simple search process", description: "We checked it out, even the kid did it, but it was my mom's friend's son" },
+        {  Image: "pictures/support.svg" , title: "24/7 Support" , description: "Is there something you don't understand? Feel free to call us. Phone number in the footer" },
+    {   Image: "pictures/mice.svg" , title: "We are nice" , description: "Fantasy is over, there will be something really convincing here" },
+    ];
+    
+    const renderContainer = why choose us => {
+        const Container = document.getElementById('chooseusContainer');
+        chooseusContainer.forEach(chooseusContainer => {
+            container.innerHTML += `
+                <div>
+                    <img src=" ${chooseusContainer.Image} " 
+                    <h3>${chooseusContainer.title}</h3>
+                    <p>${chooseusContainer.description}</p>
+                </div>
+            `;
+        });
+    }
+    
+    renderchooseusContainer(chooseusContainer);
